2dbcd1da67fc1fd830e7be53ae1fd4872b0b40e8
Huangpan <huangpan@gmail.com>
2017-01-12 11:45:49 +0800

    Initial Commit

---
+++ HPErrorViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HPErrorViewDemo
+//
+//  Created by huangpan on 17/1/12.
+//  Copyright © 2017年 Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HPErrorViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HPErrorViewDemo
+//
+//  Created by huangpan on 17/1/12.
+//  Copyright © 2017年 Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
